c22ad5915468f3214f362cc25584702c64e52051
CarbenBlue <calabenson06@gmail.com>
2025-03-13 13:42:33 -0600

    script.js

--- script.js
+++ script.js
@@ -154,3 +154,24 @@ window.addEventListener("DOMContentLoaded", () => {
   displayLocalTasks();
   fetchBitLabsSurveys();
 });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgIPKR6sUgLKaurAp6nqIXHQQQ5JiL-5Y",
+  authDomain: "tasktreasure-60d07.firebaseapp.com",
+  projectId: "tasktreasure-60d07",
+  storageBucket: "tasktreasure-60d07.firebasestorage.app",
+  messagingSenderId: "198792526809",
+  appId: "1:198792526809:web:a60a3f1ad64946d90737c8",
+  measurementId: "G-B01E3WRR9H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
